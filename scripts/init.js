182c76506c7c3ba16acaa0a0a040d2e444bae66b
Sennoma-Nn <3772564985@qq.com>
2026-06-12 02:15:32 +0800

    add new 16 button

--- scripts/init.js
+++ scripts/init.js
@@ -1,6 +1,7 @@
 (async () => {
     await loadLanguage();
     document.getElementById('new16Button').innerHTML = lang('New16', 'New (8x1<bright>6</bright>)');
+    document.getElementById('new14Button').innerHTML = lang('New14', 'New (8x1<bright>4</bright>)');
     document.getElementById('new8Button').innerHTML = lang('New8', 'New (8x<bright>8</bright>)');
     document.getElementById('renameButton').innerHTML = lang('Rename', '<bright>R</bright>ename');
     document.getElementById('openButton').innerHTML = lang('Open', '<bright>O</bright>pen');
